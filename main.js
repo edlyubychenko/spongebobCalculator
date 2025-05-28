e831c149f659c74cbaf19a76f898b41e532f6fc6
edlyubychenko <ebonylyubychenko@gmail.com>
2025-05-28 15:16:47 -0400

    jsWorking

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 // Place to show calculations
 const display = document.getElementById("display")
-
+// function to show the display 
 function showOnDisplay(input){
     display.value = input;
 }
+
+
+// function to clear display
+ function clearDisplay() {
+    display.value = "";
+ }
+
+//  function to calculate 
+ function mathOperations() {
+    display.value = eval(display.value);
+ }
